34f1f9b634f73132f1197cd127055581dfb66ce1
Savva98 <pahomovsavva@gmaail.com>
2023-08-14 17:27:02 +0300

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -13,9 +13,6 @@ import { async } from "regenerator-runtime";
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-if (module.hot) {
-  module.hot.accept();
-}
 
 const controlRecipes = async function () {
   try {
