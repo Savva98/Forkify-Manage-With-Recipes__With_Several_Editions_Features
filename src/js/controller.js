4ef315a4669b2587e921235cb7046b3b229b9ca4
Savva8030 <93856194+Savva98@users.noreply.github.com>
2023-09-19 11:40:14 +0300

    delete console.log

--- src/js/controller.js
+++ src/js/controller.js
@@ -36,7 +36,6 @@ const controlRecipes = async function () {
     //2) Rendering recipe
     recipeView.render(model.state.recipe);
     //
-    console.log(model.state.recipe);
   } catch (err) {
     recipeView.renderError();
   }
